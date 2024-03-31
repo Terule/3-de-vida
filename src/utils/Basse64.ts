c1352874235ca35273c521cfc8a38697639b1acf
Rafael Aguiar <terule@gmail.com>
2024-03-31 15:55:28 -0300

    refactor: Remove Base64 class as it is no longer needed

--- src/utils/Basse64.ts
+++
@@ -1,8 +0,0 @@
-export class Base64 {
-    public encode(data: string) {
-        return Buffer.from(data).toString("base64");
-    }
-    public decode(data: string) {
-        return Buffer.from(data, "base64").toString("utf-8");
-    }
-}
