b193f3a92a294023e47a8f1ec305cb07d3ed860d
Rafael Aguiar <terule@gmail.com>
2024-03-31 15:17:24 -0300

    feat: Add additional middleware for user creation functionality

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,7 +7,12 @@ const middlewares = new Middlewares();
 const route = Router();
 
 route.get("/", userController.getAll);
-route.post("/", middlewares.upload, userController.create);
+route.post(
+  "/",
+  middlewares.upload,
+  middlewares.checkUserData,
+  userController.create
+);
 route.get("/:id", userController.getById);
 route.put("/:id", userController.update);
 route.delete("/:id", userController.remove);
