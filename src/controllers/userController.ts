20a0e5b21bf4819d2982a273a8befec4635be2b5
Rafael Aguiar <terule@gmail.com>
2024-03-31 15:56:41 -0300

    refactor: Update variable names and use Base64 utility to encode user password

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,17 @@
-import { userData } from "@cTypes/userTypes";
+import { userCreateData } from "@cTypes/userTypes";
 import userService from "@services/userService";
+import { Base64 } from "@utils/Base64";
 import { NextFunction, Request, Response } from "express";
 import fs from "fs";
 
+const base64 = new Base64();
+
 const create = async (req: Request, res: Response, next: NextFunction) => {
-  const { email, name, password }: userData = req.body;
+  const { email, name, password }: userCreateData = req.body;
   const imageUrl = req.file ? req.file.path : null;
   try {
-    await userService.create({ email, name, password, imageUrl });
+    const encodedPassword = base64.encode(password);
+    await userService.create({ email, name, password: encodedPassword, imageUrl });
     res.status(201).json({ message: "User created" });
   } catch (e) {
     if (imageUrl) {
@@ -39,7 +43,7 @@ const getById = async (req: Request, res: Response) => {
 const update = async (req: Request, res: Response) => {
   try {
     const id: string = req.params.id;
-    const userData: userData = req.body;
+    const userData: userCreateData = req.body;
     const user = await userService.update(id, userData);
     res.status(200).json(user);
   } catch (error) {
