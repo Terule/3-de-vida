5f8ea2125b318be7c582030195d116a735112279
Rafael Aguiar <terule@gmail.com>
2024-03-31 15:16:13 -0300

    refactor: Update constructor parameter order in PrismaError class

--- src/errors/PrismaError.ts
+++ src/errors/PrismaError.ts
@@ -2,7 +2,7 @@ export class  PrismaError extends Error {
   public statusCode: number;
   public errorCode: string;
 
-  constructor(message: string, statusCode: number, errorCode: string) {
+  constructor(statusCode: number, message: string, errorCode: string) {
     super(message);
     
     this.name = "PrismaError";
