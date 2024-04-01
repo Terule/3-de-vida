66e98859263b811ac1cb659cac513d99e5598de7
Rafael Aguiar <terule@gmail.com>
2024-04-01 16:23:34 -0300

    fix(authController): Refactor error handling to use custom error class and pass errors to next function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -30,31 +30,27 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
     if (!isPasswordValid) {
       throw new AppError(401, "Invalid email or password");
     }
-    const token = jwt.sign(
-      { id: user.id },
-      jwtSecret,
-      jwtOptions
-    );
+    const token = jwt.sign({ id: user.id }, jwtSecret, jwtOptions);
     res.status(200).json({ token });
   } catch (error) {
     next(error);
   }
 };
 
-const verify = async (req: Request, res: Response) => {
+const verify = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    throw new AppError(401, "Token not provided");
   }
   try {
     const decoded = jwt.verify(token, jwtSecret) as jwtPayload;
     const user = await userService.getById(decoded.id);
     if (!user) {
-      return res.status(401).json({ message: "User not found" });
+      throw new AppError(401, "User not found");
     }
     res.status(200).json({ message: "User verified" });
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    next(error);
   }
 };
 
